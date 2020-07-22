be00b1fcb804f913b3e7179028510f0d3bf34ebe
Carly Rickarby <carlyrickarby@Carlys-MacBook-Pro.local>
2020-07-22 15:03:35 -0400

    hw chapter 2

---
+++ Chapter/2/Triangle.js
@@ -0,0 +1,11 @@
+let hashtag = "#";
+while (hashtag.length <=7) {
+  console.log(hashtag);
+  hashtag = hashtag + "#";
+}
+
+let n ="\n"
+console.log (n)
+
+for (let hashtag = "#"; hashtag.length <=7 ; hashtag = hashtag + "#")
+{console.log (hashtag);}
